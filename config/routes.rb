8cc4313c886dd589afcc2d965e049616e51a06ce
Clenio Mendes <clenio.si@gmail.com>
2017-08-28 22:49:01 +0000

    send a message

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   
-  resources :conversations, only: [:create]
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+  end
+  
+  resources :conversations, only: [:create] do
+    resources :messages, only: [:create]
+  end
 end
